bd204eda05a31197dc99cc608aff1480dc9558a6
Eamonn O'Brien <eamonnob@gmail.com>
2018-02-19 22:54:26 +0000

    Added prototypes for new style modifier properties

--- modifier.ts
+++ modifier.ts
@@ -108,25 +108,6 @@ function boosts(field: string, amount: number = 1): NewModifier.Booster {
 
 class OldModifier {
 
-	// Value is slightly different to benefit. It lets items that might not provide any direct
-	// benefit still be included in the purchase rankings and also provides a slight boost to
-	// the purchase order of things like discounts etc. Basically if a measurable benefit is 
-	// available it is used, if not, just treat it as a lump of coal.
-	get value(): number {
-		let ben: number = this.benefit;
-		if (ben > 0 || this.name == "Chocolate egg") 
-			return ben;
-		let cps: number = this.sim.effectiveCps();
-		this.sim.productionScale *= 1.01;
-		cps = this.sim.effectiveCps() - cps;
-		this.sim.productionScale /= 1.01;
-		return cps;
-	}
-
-	//
-	// Modification functions
-	//
-
 	isUnsupported(): void {
 		this.unsupported = true;
 	}
@@ -136,57 +117,13 @@ class OldModifier {
 		return this; 
 	}
 
-	scalesBuildingPrice(scale: number): this {
-		this.addApplier(() => this.sim.buildingPriceScale *= scale);
-		this.addRevoker(() => this.sim.buildingPriceScale /= scale);
-		return this;
-	}
-
-	scalesBuildingRefundRate(scale: number): this {
-		this.addApplier(() => this.sim.buildingRefundRate *= scale);
-		this.addRevoker(() => this.sim.buildingRefundRate /= scale);
-		return this;
-	}
-
-	scalesClicking(scale: number): this {
-		this.addApplier(() => this.sim.cpcMultiplier *= scale);
-		this.addRevoker(() => this.sim.cpcMultiplier /= scale);
-		return this;
-	}
-
-	scalesGoldenCookieEffectDuration(scale: number): this {
-		this.addApplier(() => this.sim.goldenCookieEffectDurationMultiplier *= scale);
-		this.addRevoker(() => this.sim.goldenCookieEffectDurationMultiplier /= scale);
-		return this;
-	}
-
-	scalesGoldenCookieFrequency(scale: number): this {
-		this.addApplier(() => this.sim.goldenCookieTime /= scale);
-		this.addRevoker(() => this.sim.goldenCookieTime *= scale);
-		return this;
-	}
-
-	scalesMilk(scale: number): this {
-		this.addApplier(() => this.sim.milkMultiplier *= scale);
-		this.addRevoker(() => this.sim.milkMultiplier /= scale);
-		return this;
-	}	
-	
-	scalesPrestige(scale: number): this {
-		this.addApplier(() => this.sim.prestigeScale *= scale);
-		this.addRevoker(() => this.sim.prestigeScale /= scale);
-		return this;
-	}
-
-	scalesProduction(scale: number): this {
-		this.addApplier(() => this.sim.productionScale *= scale);
-		this.addRevoker(() => this.sim.productionScale /= scale);
-		return this;
-	}
-
-	scalesUpgradePrice(scale: number): this {
-		this.addApplier(() => this.sim.upgradePriceScale *= scale);
-		this.addRevoker(() => this.sim.upgradePriceScale /= scale);
-		return this;
-	}
+	scalesBuildingPrice(scale: number): this				{ return this.addComponent(new NewModifier.Scaler("buildingPriceScale", scale)); }
+	scalesBuildingRefundRate(scale: number): this			{ return this.addComponent(new NewModifier.Scaler("buildingRefundRate", scale)); }
+	scalesClicking(scale: number): this						{ return this.addComponent(new NewModifier.Scaler("cpcMultiplier", scale)); }
+	scalesGoldenCookieEffectDuration(scale: number): this 	{ return this.addComponent(new NewModifier.Scaler("goldenCookieEffectDurationMultiplier", scale)); }
+	scalesGoldenCookieFrequency(scale: number): this 		{ return this.addComponent(new NewModifier.Scaler("goldenCookieTime", 1 / scale)); }
+	scalesMilk(scale: number): this 						{ return this.addComponent(new NewModifier.Scaler("milkMultiplier", scale)); }
+	scalesPrestige(scale: number): this 					{ return this.addComponent(new NewModifier.Scaler("prestigeScale", scale)); }
+	scalesProduction(scale: number): this 					{ return this.addComponent(new NewModifier.Scaler("productionScale", scale)); }
+	scalesUpgradePrice(scale: number): this					{ return this.addComponent(new NewModifier.Scaler("upgradePriceScale", scale)); }
 }
