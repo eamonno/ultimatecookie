b0ad1b298119218498a70a7c88f22ada4061ef3f
Eamonn O'Brien <eamonnob@gmail.com>
2018-02-28 23:26:52 +0000

    Added UpgradeFlags enum.

--- upgrade.ts
+++ upgrade.ts
@@ -5,3 +5,16 @@
 // 		this.modifier = new NewModifier(sim, components, name, true);
 // 	}
 // }
+
+enum UpgradeFlags {
+    Unlocked        = 0x001,
+    Unsupported     = 0x002,
+    Egg             = 0x004,
+    SantaReward     = 0x008,
+    GoldenSwitch    = 0x010,
+    SeasonChanger   = 0x020,
+    RareEgg         = 0x040,
+    Cookie          = 0x080,
+    Synergy         = 0x100,
+    Toggle          = 0x200,
+}
