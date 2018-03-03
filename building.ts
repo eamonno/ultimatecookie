d3258f7e566909438a9d3b16bd2bded0cb9836ca
Eamonn O'Brien <eamonnob@gmail.com>
2018-03-03 23:34:45 +0000

    Moved PurchasChain and Building related classes to purchase.ts and building.ts respectively.

--- building.ts
+++ building.ts
@@ -1,3 +1,5 @@
+/// <reference path="purchase.ts" />
+
 // Indices into the buildings arrays
 enum BuildingIndex {
 	Cursor = 0,
@@ -75,4 +77,146 @@ class BuildingCounter {
 	subtractCountMost(excludes: BuildingIndex[], scale: number = 1): BuildingCounter {
 		return this.addCountMost(excludes, -scale);
 	}	
+}
+
+//
+// Building.
+//
+// Represents one of the building types in the game.
+//
+
+interface BuildingSynergy {
+	index: BuildingIndex
+	scale: number
+}
+
+class Building extends Purchase {
+	quantity: number
+	level: number
+	free: number
+	multiplier: number
+	synergies: BuildingSynergy[]
+	perBuildingFlatCpcBoostCounter: BuildingCounter = new BuildingCounter();
+	perBuildingFlatCpsBoostCounter: BuildingCounter = new BuildingCounter();
+	buildingScaler: BuildingCounter = new BuildingCounter();
+	scaleCounter: BuildingCounter = new BuildingCounter();
+
+	constructor(sim: Simulator, public index: BuildingIndex, name: string, public basePrice: number, public baseCps: number) {
+		super(sim, name);
+		this.reset();
+	}
+
+	get isAvailable(): boolean { return true; }
+
+	purchase(): void {
+		//do {
+			Game.ObjectsById[this.index].buy(1);
+			this.apply();
+		//} while (this.price <= Game.cookies && this.price <= this.sim.cps);
+	}
+
+	apply() {
+		this.sim.buildings[this.index].quantity++;
+		if (this.index == BuildingIndex.Cursor) {
+			this.sim.recalculateUpgradePriceCursorScale(); 
+		}
+	}
+
+	revoke() {
+		this.sim.buildings[this.index].quantity--;
+		if (this.index == BuildingIndex.Cursor) {
+			this.sim.recalculateUpgradePriceCursorScale(); 
+		}
+	}
+
+	reset(): void {
+		this.quantity = 0;
+		this.level = 0;
+		this.free = 0;
+		this.multiplier = 1;
+		this.synergies = [];
+		this.perBuildingFlatCpcBoostCounter.clear();
+		this.perBuildingFlatCpsBoostCounter.clear();
+		this.buildingScaler.clear();
+		this.scaleCounter.clear();
+	}
+
+	nthPrice(n: number): number {
+		return Math.ceil(this.sim.buildingPriceScale * this.basePrice * Math.pow(1.15, Math.max(0, n - this.free)));
+	}
+
+	get price(): number {
+		return this.nthPrice(this.quantity);
+	}
+	
+	get cps(): number {
+		// Level multiplier only gets added to total buildings, adding it to the individual cps will cause a mismatch
+		return this.quantity * this.individualCps * this.levelMultiplier;
+	}
+
+	get individualCps(): number {
+		return this.perBuildingFlatCpsBoostCounter.getCount(this.sim.buildings) + this.baseCps * (1 + this.scaleCounter.getCount(this.sim.buildings)) * this.synergyMultiplier * (1 + this.buildingScaler.getCount(this.sim.buildings)) * this.multiplier;
+	}
+
+	get levelMultiplier(): number {
+		return 1 + (this.level * 0.01);
+	}
+
+	get longName(): string {
+		return this.name + " " + (this.quantity + 1);
+	}
+
+	get synergyMultiplier(): number {
+		let scale = 1;
+		for (let i = 0; i < this.synergies.length; ++i) {
+			scale *= 1 + this.sim.buildings[this.synergies[i].index].quantity * this.synergies[i].scale;
+		}
+		return scale;
+	}
+
+	addSynergy(index, scale): void {
+		this.synergies.push({ index, scale });
+	}
+
+	removeSynergy(index, scale): void {
+		for (let i = this.synergies.length - 1; i >= 0; --i) {
+			if (this.synergies[i].index == index && this.synergies[i].scale == scale) {
+				this.synergies.splice(i, 1);
+				return;
+			}
+		}
+	}
+
+	refundValue(index?: number): number {
+		function refundAmount(n: number): number { 
+			return Math.floor(this.nthPrice(n) * this.sim.buildingRefundRate); 
+		}
+	
+		if (index) {
+			return refundAmount(index);
+		}
+		let total: number = 0;
+		for (let i = 0; i < this.quantity; ++i) 
+			total += refundAmount(i);
+		return total;
+	}
+
+	get matchErrors(): string[] {
+		let errors: string[] = [];
+
+		let gameObj = Game.ObjectsById[this.index];
+		if (gameObj) {
+			if (this.name != gameObj.name)
+				errors.push("Building Name " + this.name + " does not match " + gameObj.name);			
+			if (this.level != gameObj.level)
+				errors.push("Building Level " + this.level + " does not match " + gameObj.level);
+			if (!floatEqual(this.price, gameObj.getPrice()))
+				errors.push("Building Cost " + this.name + " - Predicted: " + this.price + ", Actual: " + gameObj.getPrice());
+			if (!floatEqual(this.individualCps, gameObj.cps(gameObj)))
+				errors.push("Building CpS " + this.name + " - Predicted: " + this.individualCps + ", Actual: " + gameObj.cps(gameObj));
+		} else {
+			errors.push("Building Index " + this.index + " doesn't match any building.");
+		}
+		return errors;
+	}
 }
--- purchase.ts
+++ purchase.ts
@@ -1,3 +1,5 @@
+/// <reference path="modifier.ts" />
+
 //
 // Purchases.
 //
@@ -26,3 +28,61 @@ abstract class Purchase extends Modifier {
 		return this.value / this.price;
 	}
 }
+
+//
+// PurchaseChain
+//
+// Looking ahead multiple purchases can give better results. This class is designed to represent
+// those chains of purchases.
+//
+
+class PurchaseChain extends Purchase {
+	constructor(sim: Simulator, public purchases: Purchase[]) {
+		super(sim, purchases.map(p => p.name).join(" -> "));
+	}
+
+	get isAvailable(): boolean {
+		return this.purchases.every(p => p.isAvailable);
+	}
+
+	apply(): void {
+		for (let i = 0; i < this.purchases.length; ++i) {
+			this.purchases[i].apply();
+		}
+	}
+
+	revoke(): void {
+		for (let i = this.purchases.length - 1; i >= 0; --i) {
+			this.purchases[i].revoke();
+		}
+	}
+
+	get longName(): string {
+		return this.purchases.map(p => p.longName).join(" -> ");
+	}
+
+	get purchaseTime(): number {
+		let time: number = 0;
+		for (var i = 0; i < this.purchases.length; ++i) {
+			time += this.purchases[i].purchaseTime;
+			this.purchases[i].apply();
+		}
+		this.revoke();
+		return time;
+	}
+
+	purchase(): void {
+		console.log("Attempt to buy a PurchaseChain");
+	}
+
+	get price(): number {
+		let price = 0;
+		for (let i = 0; i < this.purchases.length; ++i) {
+			price += this.purchases[i].price;
+			this.purchases[i].apply();
+		}
+		this.revoke();
+		return price;
+	}
+}
+
